ba3c63d3f4838e2c908f3804bd7c33d2f95c8df7
Vijaya Kumar V <vulchi.vijay@gmail.com>
2021-07-30 23:14:46 +0530

    backend libraries added in server.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 
 // import routes
 const authRoutes = require('./routes/auth.js');
 
+// app middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+// app.use(cors()); // allows all origins
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(cors({origin: 'http://localhost:3000'}));
+}
+
 // middleware
 app.use('/api', authRoutes);
 
 // port
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`API is running on port ${port}`);
+  console.log(`API is running on port ${port} - ${process.env.NODE_ENV}`);
 })
