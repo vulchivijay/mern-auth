dc370e57cd3ae409b86637068cd4edf87b9366c4
Vijaya Kumar V <vulchi.vijay@gmail.com>
2021-07-31 12:34:38 +0530

    Backend sending mail to user to activate the account
    email verification not delivered to user, need to look that. in sendgrid mails are blocked in email activities.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,11 @@
 // Controllers
 const User = require('./../models/user');
+const jwt = require('jsonwebtoken');
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
 exports.signup = (req, res) => {
-  // console.log('Signup body :', req.body);
-  // res.json({
-  //   data: 'you hit sign up end point !'
-  // })
   const { name, email, password} = req.body;
   User.findOne({ email }).exec((error, user) => {
     if (user) {
@@ -14,17 +14,29 @@ exports.signup = (req, res) => {
       })
     }
   })
-  let newUser = new User({name, email, password});
-  newUser.save((error, success) => {
-    if(error) {
-      console.log('Signup error: ', error);
-      return res.status(400).json({
-        error: error
-      })
-    }
-    res.json({
-      message: 'Signup success! Please signin.'
+
+  const token = jwt.sign({name, email, password}, process.env.JWT_ACCOUNT_ACTIVATION, {expiresIn: '10m'});
+  const emailData = {
+    from: process.env.EMAIL_FROM,
+    to: email,
+    subject: `Account activation link`,
+    html: `
+      <p>Please use the following link to activate your account</p>
+      <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+      <hr/>
+      <p>This email may contain sensetive information</p>
+      <p>${process.env.CLIENT_URL}</p>`
+  }
+  sgMail.send(emailData).then(sent => {
+    // console.log('Signup mail sent', sent);
+    return res.json({
+      message: `Email has been sent to ${email}. Follow the instructions to activate your account`
     })
   })
-};
-
+  .catch(error => {
+    // console.log('Signup email sent error :', error);
+    return res.json({
+      message: error.message
+    })
+  })
+}
