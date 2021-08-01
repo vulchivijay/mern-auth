f04a9ed5fd521f6b53bd891f6a552f487daf90b4
Vijaya Kumar V <vulchi.vijay@gmail.com>
2021-08-01 11:06:32 +0530

    Frontend - Active color added for main menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-export default function Header () {
+function Header (match) {
+  const isActive = path => {
+    if (match.match.path === path){
+      return { color: '#39d000' }
+    } else {
+      return { color: '#000000'}
+    }
+  }
+
   return (
     <header>
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <Link to='/' className="navbar-brand mb-0 h1">Logo</Link>
           <div className="d-flex">
-            <Link to='/signup' className="nav-link">Signup</Link>
-            <Link to='/signin' className="nav-link">Signin</Link>
+            <Link to='/signup' className="nav-link" style={ isActive('/signup') }>Signup</Link>
+            <Link to='/signin' className="nav-link" style={ isActive('/signin') }>Signin</Link>
           </div>
         </div>
       </nav>
     </header>
   );
 }
+
+export default withRouter(Header);
