4f27c4ffaacb5cfa8a0057bd76a59a97c7272cfc
Vijaya Kumar V <vulchi.vijay@gmail.com>
2021-08-08 14:40:23 +0530

    initial Screenshots of all the pages

--- src/components/admin/Aside.js
+++ src/components/admin/Aside.js
@@ -9,7 +9,7 @@ export default function AsideBar () {
         isAuth().role === 'admin' ? <Link to='/dashboardadmin' className="nav-link px-2 link-dark">Dashboard</Link> : <Link to='/dashboard' className="nav-link px-2 link-dark">Dashboard</Link>
       }
       <Link to='/newtask' className="nav-link px-2 link-dark">New task</Link>
-      <Link to='/alltasks' className="nav-link px-2 link-dark">View tasks</Link>
+      <Link to='/alltasks' className="nav-link px-2 link-dark">All tasks</Link>
       <Link to='/profile' className="nav-link px-2 link-dark">Profile</Link>
       {
         isAuth().role === 'admin' ? <Link to='/users' className="nav-link px-2 link-dark">Users</Link> : null
