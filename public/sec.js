3b14a019040f858ae261e26d48a5feb00630e7c4
Vijaya Kumar V <vulchi.vijay@gmail.com>
2021-08-08 20:40:10 +0530

    package info update

---
+++ public/sec.js
@@ -0,0 +1,17 @@
+// Front env data
+REACT_APP_API = http://localhost:8000/api
+
+// Backend env data
+// Environment variables
+
+NODE_ENV = development
+PORT = 8000
+CLIENT_URL = http://localhost:3000
+DATABASE_URL = 'mongodb://localhost:27017/mernauth'
+SMTP_HOST = smtp.mailtrap.io
+SMTP_PORT = 2525
+MAIL_USERNAME = bef35cb8e92c22
+MAIL_PASSWORD = c5a28bc666ad46
+JWT_SECRET = EDASD54DF8AD5FASD4545SDF55DFA141DSD
+JWT_ACCOUNT_ACTIVATION = S5D47D8ADFSDF4ASD5FFSD2F1SD5FSDF2SS4DF
+EMAIL_FROM = vulchi.vijay@zohomail.com
